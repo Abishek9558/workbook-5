baeff4bdc772af97fe680545e119c918a6814d80
Abishek Aryal <abishekaryal9558@gmail.com>
2025-05-13 15:48:23 -0400

    AssetManager working

--- AssetManager/src/main/java/com/pluralsight/Asset.java
+++ AssetManager/src/main/java/com/pluralsight/Asset.java
@@ -1,10 +1,11 @@
 package com.pluralsight;
 
-public class Asset {private String description;
+public class Asset {
+    private String description;
     private String dateAcquired;
     private double originalCost;
 
-    public AssetManager(String description, String dateAcquired, double originalCost) {
+    public Asset(String description, String dateAcquired, double originalCost) {
         this.description = description;
         this.dateAcquired = dateAcquired;
         this.originalCost = originalCost;
@@ -26,12 +27,17 @@ public class Asset {private String description;
         this.dateAcquired = dateAcquired;
     }
 
-    public void setOriginalCost(double originalCost) {
-        this.originalCost = originalCost;
-    }
-
     public double getOriginalCost() {
         return originalCost;
     }
 
+    public void setOriginalCost(double originalCost) {
+        this.originalCost = originalCost;
+    }
+
+    public double getValue() {
+        return originalCost;
+    }
 }
+
+
